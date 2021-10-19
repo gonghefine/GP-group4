a1aec699133beff329b14bc05810ce55fe9dcd90
gonghefine <92682888+gonghefine@users.noreply.github.com>
2021-10-19 20:58:00 +0800

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path('emissions/<int:page>', app.views.emissions, name='emissions'),
     path('aggregation/', app.views.aggregation, name='aggregation'),
     path('aggregation/<int:page>', app.views.aggregation, name='aggregation'),
-    path('visual/', app.views.visual, name='visual'),
+    path('pie_chart/', app.views.pie_chart, name='pie_chart'),
     path('emissions/imo/', app.views.emission_detail, name='emission_detail'),
     path('emissions/imo/<int:imo>', app.views.emission_detail, name='emission_detail'),
     path('admin/', admin.site.urls),
