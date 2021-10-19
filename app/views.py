ebc6a36dc2fa27d5071ba891f147d2a4dd19bb0b
gonghefine <92682888+gonghefine@users.noreply.github.com>
2021-10-19 15:22:50 +0800

    Update views.py

--- app/views.py
+++ app/views.py
@@ -79,12 +79,10 @@ def aggregation(request, page=1):
     order_by = order_by if order_by in COLUMNS else 'imo'
 
     with connections['default'].cursor() as cursor:
-        cursor.execute('SELECT cr.ship_type, min(cr.technical_efficiency_number), max(cr.technical_efficiency_number), AVG(cr.technical_efficiency_number FROM co2emission_reduced cr GROUP BY cr.ship_type')
+        cursor.execute('SELECT cr.ship_type AS ship_type, min(cr.technical_efficiency_number) AS min, max(cr.technical_efficiency_number) AS max, AVG(cr.technical_efficiency_number) AS avg FROM co2emission_reduced cr GROUP BY cr.ship_type')
         count = cursor.fetchone()[0]
         num_pages = (count - 1) // PAGE_SIZE + 1
         page = clamp(page, 1, num_pages)
-
-       
         rows = namedtuplefetchall(cursor)
 
     imo_deleted = request.GET.get('deleted', False)
